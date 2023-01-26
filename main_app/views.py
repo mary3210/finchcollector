e74c37b4054b0c756badde9695854d74f2aab64c
mary3210 <maryrodri3210@gmail.com>
2023-01-25 22:29:07 -0600

    initialized git repo

---
+++ main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.views.generic.base import TemplateView
+# Create your views here.
+class Index:
+    def __init__(self, name, image, bio):
+        self.name = name
+        self.image = image
+        self.bio = bio
+
+Indexes = [
+  Index("Vitelotte", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Vitelotte.jpg/1200px-Vitelotte.jpg",
+          "a gourmet French variety of blue-violet potato. It has been cultivated in France at least since the early nineteenth century."),
+  Index("Bintje",
+          "https://harvesting-history.com/wp-content/uploads/2017/01/HH_Potato_Bintje.jpg", "Bintje is a middle-early ripening potato variety bred in the Netherlands by the Frisian schoolmaster K.L. de Vries in 1904 from and marketed for the first time in 1910. "),
+  Index("Kerr's Pink", "http://specialtyproduce.com/sppics/16382.png",
+          "a potato cultivar in wide production in Ireland and the United Kingdom and many other countries. Although often quoted as an 'Irish potato', the cultivar was actually created by J. Henry of Cornhill, Scotland, in 1907."),                       
+]
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class Index(TemplateView):
+    template_name = "index.html"

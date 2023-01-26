e633c33250c927f1cc8e37f91a1b05cac708d518
mary3210 <maryrodri3210@gmail.com>
2023-01-26 14:14:39 -0600

    populated images on index route

--- main_app/views.py
+++ main_app/views.py
@@ -25,3 +25,7 @@ class About(TemplateView):
 
 class Index(TemplateView):
     template_name = "index.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Indexes"] = Indexes # this is where we add the key into our context object for the view to use
+        return context

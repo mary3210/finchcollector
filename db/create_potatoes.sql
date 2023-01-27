bb63b89f8420f4d467cdcad24075e91ce9e2c26d
mary3210 <maryrodri3210@gmail.com>
2023-01-27 08:07:37 -0600

    added deb file

---
+++ db/create_potatoes.sql
@@ -0,0 +1,11 @@
+INSERT INTO main_app_index(name, img, bio, created_at)
+  VALUES
+    ('Russian blue potato','https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Russian_Blue_potato.jpg/800px-Russian_Blue_potato.jpg','Russian blues are rich in anthocyanin, vitamin C, potassium, and fiber.[6] The potatoes retain their colour when cooked[7] and have a similar texture to russet potatoes','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_index(name, img, bio, created_at)
+  VALUES
+    ('Ratte potato','https://specialtyproduce.com/sppics/5339.png','A small potato with a unique nutty flavor and smooth, buttery texture.','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_index(name, img, bio, created_at)
+  VALUES
+    ('King Edward','http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRGYwgXMk_kjsOkl1IueTRPdvuCM61-aI5dWgk4cDYiw4Ioo7SpT4YpxaEP2VJltvWWx_wIP4kIY7jm3GY',' It has been described by Delia Smith as being the best potato with which to make gnocchi.','2021-05-07 18:57:42.517205-07');
--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Index
 # Register your models here.
+admin.site.register(Index)
